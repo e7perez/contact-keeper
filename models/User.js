36713d64a4f61a4331ca6fa99fa782b1f4ffbd4d
Emmanuel Perez <emmanueleperez>
2021-03-12 18:19:33 -0500

    Added user model and validation

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String, 
+        requred: true
+    },
+    email: {
+        type: String, 
+        requred: true,
+        unique: true
+    },
+    password: {
+        type: String, 
+        requred: true
+    },
+    date: {
+        type: String, 
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema);
